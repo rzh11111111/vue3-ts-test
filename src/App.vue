a770ccfc556fe038c025571651eba8bb3f452560
任宗豪 <30588887+rzh11111111@users.noreply.github.com>
2022-03-12 16:16:22 +0800

    fix：添加readme

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+ 
+<div>{{message+1}}</div>
+<div>{{count}}</div>
 </template>
 
+<script setup lang="ts">
+import {ref} from 'vue'
+const message:string = '123123'
+const count = ref(0)
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
